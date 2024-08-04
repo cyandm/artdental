566e4157d71f45a5044dd81463aff6efba328e42
fateme-bakshiri <f.bakshiri@gmail.com>
2024-08-04 11:25:10 +0330

    functions.php edited

--- functions.php
+++ functions.php
@@ -9,6 +9,7 @@ require_once(__DIR__ . '/inc/classes/cyn-theme-init.php');
 require_once(__DIR__ . '/inc/classes/cyn-customize.php');
 require_once(__DIR__ . '/inc/classes/cyn-register.php');
 require_once(__DIR__ . '/inc/classes/cyn-search.php');
+require_once(__DIR__ . '/inc/classes/cyn-custom-code.php');
 
 
 //functions
@@ -27,3 +28,4 @@ new cyn_theme_init(false, '0.0.0');
 new cyn_register();
 new cyn_customize();
 new cyn_search();
+new cyn_custom_code();
