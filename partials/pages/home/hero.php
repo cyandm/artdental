97346e4165efbdea2037ad51284f8e655b9293a8
fateme-bakshiri <f.bakshiri@gmail.com>
2024-10-05 17:15:43 +0330

    put logo after hero title

--- partials/pages/home/hero.php
+++ partials/pages/home/hero.php
@@ -2,8 +2,13 @@
 
     <div class="p-4 bg-background-card_1 rounded-2xl text-primary-30 space-y-4 block lg:hidden fade-in">
         <div class="space-y-2">
-            <div class="text-[72px] max-xl:text-[52px] max-md:text-h3 font-extrabold">
-                <?php echo get_field('hero_title') ?>
+            <div class="flex justify-between items-center">
+                <div class="text-[72px] max-xl:text-[52px] max-md:text-h3 font-extrabold">
+                    <?php echo get_field('hero_title') ?>
+                </div>
+                <div class="logo size-10">
+                    <?php the_custom_logo() ?>
+                </div>
             </div>
 
             <div class="text-[58px] max-2xl:text-h2 max-2xl:font-semibold max-xl:text-h3 max-md:text-h4 max-md:font-normal max-sm:text-h5">
@@ -45,8 +50,13 @@
 
         <div anim-delay="0.5" class="p-4 bg-background-card_1 rounded-2xl text-primary-30 space-y-4 hidden lg:block fade-in-down">
             <div class="space-y-2">
-                <div class="text-[72px] max-xl:text-[52px] font-extrabold">
-                    <?php echo get_field('hero_title') ?>
+                <div class="flex justify-between text-center items-center">
+                    <div class="text-[72px] max-xl:text-[52px] font-extrabold">
+                        <?php echo get_field('hero_title') ?>
+                    </div>
+                    <div class="logo size-10 justify-end items-end">
+                        <?php the_custom_logo() ?>
+                    </div>
                 </div>
 
                 <div class="text-[58px] max-2xl:text-h2 max-2xl:font-semibold max-xl:text-h3">
@@ -68,7 +78,7 @@
             </div>
         </div>
         <div class="fade-in-down cursor-pointer" anim-delay="0.8">
-            <cyn-cta class="reservePopUpOpener"/>
+            <cyn-cta class="reservePopUpOpener" />
         </div>
 
 
